dd268befdc466935ec448b5c6a8ea20312c0b737
good-GOD <3081974933@qq.com>
2021-01-27 16:31:53 +0800

    test

---
+++ src/test1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class test1 extends Demo{
+    public static void main(String[] args) {
+        System.out.println("6,12的最大值为："+maxdata(6,12));
+        System.out.println("6,12,10的最大值为："+maxdata(6,12,10));
+        System.out.println("19.35,28.99,65.99的最大值为："+maxdata(19.35,28.99,65.99));
+        System.out.println("9.58,18.65的最大值为："+maxdata(9.58,18.65));
+    }
+}
+
