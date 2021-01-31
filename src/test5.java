052b319db68aee8eb139dec86f9fe4959625ecbd
good-GOD <3081974933@qq.com>
2021-01-31 18:43:11 +0800

    人口数量

---
+++ src/test5.java
@@ -0,0 +1,9 @@
+public class test5 {
+    public static void main(String[] args) {
+        String[] str = {"世界人口","中国人口"};
+        long [] s = {7100000000l,1400000000l};
+        for (int i = 0; i <= 1; i++){
+            System.out.println("2019年"+str[i]+"总数约："+s[i]);
+        }
+    }
+}
