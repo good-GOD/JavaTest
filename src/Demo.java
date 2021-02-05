3985f4df62a57708da1ae5c082cd53c610dc3245
good-GOD <3081974933@qq.com>
2021-02-05 19:24:32 +0800

    取得最大值

--- src/Demo.java
+++ src/Demo.java
@@ -1,19 +1,19 @@
 public class Demo {
-    public static int maxdata(int a,int b){
+    public static int maxdate(int a,int b){
 //        if(a<b)
 //            return b;
 //        else
 //            return a;
         return Math.max(a,b);
     }
-    public static int maxdata(int a,int b,int c){
+    public static int maxdate(int a,int b,int c){
         int max1 = Math.max(a,b);
         return Math.max(max1,c);
     }
-    public static double maxdata(double a,double b){
+    public static double maxdate(double a,double b){
        return Math.max(a,b);
     }
-    public static double maxdata(double a,double b,double c){
+    public static double maxdate(double a,double b,double c){
         double max1 = Math.max(a,b);
         return Math.max(max1,c);
     }
